ffae0de2326dd7ec4d77045db0e22912eafd4e1e
Joe Buhlig <hello@joebuhlig.com>
2018-04-09 13:28:10 -0500

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,31 @@
+# name: discourse-sanitize-header-tag
+# author: Joe Buhlig
+# about: Strips the header markdown conversion
+# version: 0.1
+# author: Joe Buhlig joebuhlig.com
+# url: https://www.github.com/ha-tech/dicourse-sanitize-header-tag
+
+module ::PrettyText
+  module PrettyTextExtension
+    def markdown(text, opts = {})
+      original = super(text, opts)
+
+      original.gsub! "<h1>", "&lt;h1&gt;"
+      original.gsub! "<h2>", "&lt;h2&gt;"
+      original.gsub! "<h3>", "&lt;h3&gt;"
+      original.gsub! "<h4>", "&lt;h4&gt;"
+      original.gsub! "<h5>", "&lt;h5&gt;"
+      original.gsub! "<h6>", "&lt;h6&gt;"
+
+      original.gsub! "</h1>", "&lt;\/h1&gt;<br>"
+      original.gsub! "</h2>", "&lt;\/h2&gt;<br>"
+      original.gsub! "</h3>", "&lt;\/h3&gt;<br>"
+      original.gsub! "</h4>", "&lt;\/h4&gt;<br>"
+      original.gsub! "</h5>", "&lt;\/h5&gt;<br>"
+      original.gsub! "</h6>", "&lt;\/h6&gt;<br>"
+
+      original
+    end
+  end
+  singleton_class.prepend PrettyTextExtension
+end
