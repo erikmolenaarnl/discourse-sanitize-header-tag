f799ab358e4c3bf33c7a7c701cd6e892cc6470a9
Joe Buhlig <hello@joebuhlig.com>
2018-04-10 14:41:08 -0500

    Catch <hr> and setext headers

--- plugin.rb
+++ plugin.rb
@@ -40,21 +40,45 @@ module ::PrettyText
       text.gsub! "\n## ", "\n&#35\;&#35\; "
       text.gsub! "\n# ", "\n&#35\; "
 
+      text.gsub! "<h1>", "&lt;h1&gt;"
+      text.gsub! "<h2>", "&lt;h2&gt;"
+      text.gsub! "<h3>", "&lt;h3&gt;"
+      text.gsub! "<h4>", "&lt;h4&gt;"
+      text.gsub! "<h5>", "&lt;h5&gt;"
+      text.gsub! "<h6>", "&lt;h6&gt;"
+
+      text.gsub! "</h1>", "&lt;\/h1&gt;"
+      text.gsub! "</h2>", "&lt;\/h2&gt;"
+      text.gsub! "</h3>", "&lt;\/h3&gt;"
+      text.gsub! "</h4>", "&lt;\/h4&gt;"
+      text.gsub! "</h5>", "&lt;\/h5&gt;"
+      text.gsub! "</h6>", "&lt;\/h6&gt;"   
+      
+      text.gsub! "\n-----", "\n&#45;&#45;&#45;&#45;&#45;"
+      text.gsub! "\n----", "\n&#45;&#45;&#45;&#45;"
+      text.gsub! "\n---", "\n&#45;&#45;&#45;"
+      text.gsub! "\n--", "\n&#45;&#45;"
+      text.gsub! "\n-", "\n&#45;"
+
+      text.gsub! "\n=====", "\n&#61;&#61;&#61;&#61;&#61;"
+      text.gsub! "\n====", "\n&#61;&#61;&#61;&#61;"
+      text.gsub! "\n===", "\n&#61;&#61;&#61;"
+      text.gsub! "\n==", "\n&#61;&#61;"
+      text.gsub! "\n=", "\n&#61;"
+      
+      text.gsub! "\n_____", "\n&#95;&#95;&#95;&#95;&#95;"
+      text.gsub! "\n____", "\n&#95;&#95;&#95;&#95;"
+      text.gsub! "\n___", "\n&#95;&#95;&#95;"
+      text.gsub! "\n__", "\n&#95;&#95;"
+      text.gsub! "\n_", "\n&#95;"
+      
+      text.gsub! "\n*****", "\n&#42;&#42;&#42;&#42;&#42;"
+      text.gsub! "\n****", "\n&#42;&#42;&#42;&#42;"
+      text.gsub! "\n***", "\n&#42;&#42;&#42;"
+      text.gsub! "\n**", "\n&#42;&#42;"
+      text.gsub! "\n*", "\n&#42;"
+      
       original = super(text, opts)
-
-      original.gsub! "<h1>", "&lt;h1&gt;"
-      original.gsub! "<h2>", "&lt;h2&gt;"
-      original.gsub! "<h3>", "&lt;h3&gt;"
-      original.gsub! "<h4>", "&lt;h4&gt;"
-      original.gsub! "<h5>", "&lt;h5&gt;"
-      original.gsub! "<h6>", "&lt;h6&gt;"
-
-      original.gsub! "</h1>", "&lt;\/h1&gt;<br><br>"
-      original.gsub! "</h2>", "&lt;\/h2&gt;<br><br>"
-      original.gsub! "</h3>", "&lt;\/h3&gt;<br><br>"
-      original.gsub! "</h4>", "&lt;\/h4&gt;<br><br>"
-      original.gsub! "</h5>", "&lt;\/h5&gt;<br><br>"
-      original.gsub! "</h6>", "&lt;\/h6&gt;<br><br>"
 
       original
     end
