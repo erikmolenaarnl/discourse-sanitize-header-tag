0adaa0e4261b184b11bee63ce007e83ce7b72014
Joe Buhlig <hello@joebuhlig.com>
2018-04-10 11:57:13 -0500

    Capture markdown version and add line breaks

--- plugin.rb
+++ plugin.rb
@@ -8,6 +8,38 @@
 module ::PrettyText
   module PrettyTextExtension
     def markdown(text, opts = {})
+      
+      if text.start_with?("###### ")
+        text.sub! "###### ", "&#35\;&#35\;&#35\;&#35\;&#35\;&#35\; "
+      end
+
+      if text.start_with?("##### ")
+        text.sub! "##### ", "&#35\;&#35\;&#35\;&#35\;&#35\; "
+      end      
+      
+      if text.start_with?("#### ")
+        text.sub! "#### ", "&#35\;&#35\;&#35\;&#35\; "
+      end
+
+      if text.start_with?("### ")
+        text.sub! "### ", "&#35\;&#35\;&#35\; "
+      end
+
+      if text.start_with?("## ")
+        text.sub! "## ", "&#35\;&#35\; "
+      end
+
+      if text.start_with?("# ")
+        text.sub! "# ", "&#35\; "
+      end
+
+      text.gsub! "\n######", "\n&#35\;&#35\;&#35\;&#35\;&#35\;&#35\; "
+      text.gsub! "\n#####", "\n&#35\;&#35\;&#35\;&#35\;&#35\; "
+      text.gsub! "\n####", "\n&#35\;&#35\;&#35\;&#35\; "
+      text.gsub! "\n### ", "\n&#35\;&#35\;&#35\; "
+      text.gsub! "\n## ", "\n&#35\;&#35\; "
+      text.gsub! "\n# ", "\n&#35\; "
+
       original = super(text, opts)
 
       original.gsub! "<h1>", "&lt;h1&gt;"
@@ -17,12 +49,12 @@ module ::PrettyText
       original.gsub! "<h5>", "&lt;h5&gt;"
       original.gsub! "<h6>", "&lt;h6&gt;"
 
-      original.gsub! "</h1>", "&lt;\/h1&gt;<br>"
-      original.gsub! "</h2>", "&lt;\/h2&gt;<br>"
-      original.gsub! "</h3>", "&lt;\/h3&gt;<br>"
-      original.gsub! "</h4>", "&lt;\/h4&gt;<br>"
-      original.gsub! "</h5>", "&lt;\/h5&gt;<br>"
-      original.gsub! "</h6>", "&lt;\/h6&gt;<br>"
+      original.gsub! "</h1>", "&lt;\/h1&gt;<br><br>"
+      original.gsub! "</h2>", "&lt;\/h2&gt;<br><br>"
+      original.gsub! "</h3>", "&lt;\/h3&gt;<br><br>"
+      original.gsub! "</h4>", "&lt;\/h4&gt;<br><br>"
+      original.gsub! "</h5>", "&lt;\/h5&gt;<br><br>"
+      original.gsub! "</h6>", "&lt;\/h6&gt;<br><br>"
 
       original
     end
